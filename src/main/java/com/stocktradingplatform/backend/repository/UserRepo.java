4c7b780d349cb34f511ed8f6513f76c86cdad04c
Kiran Kodali <kirankodali10@icloud.com>
2023-11-10 10:28:29 -0800

    11-10-2023 10:28 login API

--- src/main/java/com/stocktradingplatform/backend/repository/UserRepo.java
+++ src/main/java/com/stocktradingplatform/backend/repository/UserRepo.java
@@ -1,8 +1,6 @@
 package com.stocktradingplatform.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.stocktradingplatform.backend.entity.UserEntity;
