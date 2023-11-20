4e2ca20db36dae7a3becd538665499084d6a37ac
Nisarg Bhalavat <nisargbhalavat98@gmail.com>
2023-11-20 00:21:31 -0800

    stock controller

--- src/main/java/com/stocktradingplatform/backend/controller/StockController.java
+++ src/main/java/com/stocktradingplatform/backend/controller/StockController.java
@@ -1,20 +1,32 @@
 package com.stocktradingplatform.backend.controller;
 
-import com.stocktradingplatform.backend.bean.StockBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.stocktradingplatform.backend.bean.StockBean;
+import com.stocktradingplatform.backend.service.StockService;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/stocks")
 public class StockController {
 
-//    @GetMapping("/all")
-//    public ResponseEntity<List<StockBean>> getAllUsers() {
-//        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-//    }
+    @Autowired
+    StockService stockService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<StockBean>> getAllStocks() {
+        return new ResponseEntity<>(stockService.getAllStockData(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<List<StockBean>> getStockData(@PathVariable String symbol) {
+        return new ResponseEntity<>(stockService.getStockData(symbol), HttpStatus.OK);
+    }
 }
--- src/main/java/com/stocktradingplatform/backend/repository/StockRepoWrapper.java
+++ src/main/java/com/stocktradingplatform/backend/repository/StockRepoWrapper.java
@@ -4,12 +4,16 @@ import com.stocktradingplatform.backend.bean.StockBean;
 import com.stocktradingplatform.backend.bean.UserBean;
 import com.stocktradingplatform.backend.entity.StockEntity;
 import com.stocktradingplatform.backend.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class StockRepoWrapper {
 
+    @Autowired
     StockRepo stockRepo;
 
     public List<StockBean> getAllStockData() {
